ca8c7a0f9da62af990001d0c9345d6898511e14c
Abey Onalaja <Abey.onalaja@gmail.com>
2019-12-29 21:05:09 +0000

    set default route

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,39 @@
 use hyper::service::{service_fn, service_fn_ok};
-use hyper::{Body, Response, Server};
-use hyper::rt::Future;
+use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
+use futures::{future, Future};
+
+type UserId = u64;
+struct UserData;
+
+
+const INDEX: &'static str = r#"
+ <!doctype html>
+ <html>
+     <head>
+         <title>Rust Microservice</title>
+     </head>
+     <body>
+         <h3>Rust Microservice</h3>
+     </body>
+ </html>
+ "#;
+
+fn microservice_handler(req: Request<Body>) -> impl Future<Item=Response<Body>, Error=Error>
+{
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            future::ok(Response::new(INDEX.into()))
+        },
+        _ => {
+            let response = Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::empty())
+                .unwrap();
+            future::ok(response)
+        },
+    }
+}
 
 fn main() {
 
@@ -12,11 +45,7 @@ fn main() {
 //    let server = server.map_err(drop);
 
 
-    let server = builder.serve(|| {
-        service_fn_ok(|_| {
-            Response::new(Body::from("Almost microservice..."))
-        })
-    });
+    let server = builder.serve(|| service_fn(microservice_handler));
 
     let server = server.map_err(drop);
     hyper::rt::run(server);
